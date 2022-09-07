95e5630adeed47af24c1fac7d580eabfdd1dfe27
AidanSgar <aidanrsgarlato@gmail.com>
2022-09-07 16:22:15 -0700

    Added bigining stages of the showTensor builtin

--- TensorCalculus.py
+++ TensorCalculus.py
@@ -1,17 +1,19 @@
 import numpy as np
 import string
 
-def printIndicies(a,n,m,state):
+
+def printIndicies(a, n, m, state):
     # if state is equal too True then we do contravariant and if False we do covariant
     letters = list(string.ascii_lowercase)
     if state:
         a += "^("
     else:
         a += "_("
-    for i in range(m,n+m):
+    for i in range(m, n + m):
         a += letters[i]
     a += ")"
     return a
+
 
 class tensor:
     def __init__(self, dim, sig, type, coeficents):
@@ -29,12 +31,13 @@ class tensor:
         self.type = type
 
         a = np.array(coeficents)
-        if a.ndim != type[0] + type[1]:
+        b = [dim] * (type[0] + type[1])
+        if a.shape != tuple(b):
             raise ValueError(
-                "Rank of coeficents array does not alighn with tensor type -- type: " + str(type) + " rank: " + str(
-                    a.ndim))
+                "Coeficents array does not alighn with tensor type and dimension -- type: " + str(
+                    type) + " dimension: " + str(dim) + " so the array needs to have dimensions:" + str(
+                    b) + "and currently is " + str(a.shape))
         self.coeficents = coeficents
-
 
     # Name of tensor can be used for debuging and printing, is optional and if not inputed then will be set too "T": string
     def setName(self, a):
@@ -47,20 +50,25 @@ class tensor:
 
     def showTensor(self):
         tensorString = ""
+
+        # Adding Tensor name including indicies to the start of the print out
         headerName = self.getName()
 
         if self.type[0] != 0 or self.type[1] != 0:
             if self.type[1] == 0:
-                tensorString += printIndicies(headerName,self.type[0],0,True) + " =\n\n"
+                tensorString += printIndicies(headerName, self.type[0], 0, True) + " =\n\n"
             if self.type[0] == 0:
-                tensorString += printIndicies(headerName,self.type[1],0,False) + " =\n\n"
+                tensorString += printIndicies(headerName, self.type[1], 0, False) + " =\n\n"
             if self.type[0] != 0 and self.type[1] != 0:
-                tensorString += printIndicies(printIndicies(headerName,self.type[0],0,True),self.type[1],self.type[0],False) + " =\n\n"
+                tensorString += printIndicies(printIndicies(headerName, self.type[0], 0, True), self.type[1],
+                                              self.type[0], False) + " =\n\n"
         else:
             tensorString = headerName + " =\n\n"
 
+        # Adding the coeficents to the print out with dynamic formatting
         if self.type[0] + self.type[1] == 0:
             tensorString += str(self.coeficents)
+
         if self.type[0] + self.type[1] == 1:
             if self.type[0] == 1:
                 for i in range(self.dim):
@@ -69,11 +77,30 @@ class tensor:
                 for i in range(self.dim):
                     tensorString += str(self.coeficents[i]) + "  "
 
+        if self.type[0] + self.type[1] == 2:
+            tempPosSave = [[False]*self.dim]*self.dim
+            length = [" "] * self.dim
+            for i in range(self.dim):
+                for j in range(self.dim):
+                    if len(str(self.coeficents[i][j])) > len(length[i]):
+                        length[j] = " "*(len(str(self.coeficents[i][j])))
+                        print("test",i,j)
+                        tempPosSave[i][j] = True
+
+            posSave = list(np.array(tempPosSave))
+            for i in range(self.dim):
+                for j in range(self.dim):
+                    if posSave[i][j] or j == self.dim-2:
+                        tensorString += str(self.coeficents[i][j]) + " "
+                    else:
+                        tensorString += str(self.coeficents[i][j]) + length[j]
+                tensorString += "\n"
+        print(length)
+
         print(tensorString)
 
 
-
-
 if __name__ == '__main__':
-    a = tensor(3,(1,1,1),(0,1),[1,2,3])
-    a.showTensor()
+    a = [[1000000000000, 1, 1,1], [1, 1, 10000,1], [1, 1, 1,1],[1,1,1,1]]
+    test = tensor(4, (1, 1, 1, 1), (1, 1), a)
+    test.showTensor()
